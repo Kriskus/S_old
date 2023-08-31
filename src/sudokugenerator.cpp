6be2e3d2f1e410be1dccbee63b228a2feb8fc96a
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-08-31 13:31:41 +0200

    Working on sudoku board
    Creating class for sudoku board generate

--- src/sudokugenerator.cpp
+++ src/sudokugenerator.cpp
@@ -1,7 +1,108 @@
 #include "sudokugenerator.h"
 
+#include <random>
 
 Sudoku::Sudoku()
 {
+    board_ = QVector<QVector<int>>(9, QVector<int>(9, 0));
+}
 
+Sudoku::~Sudoku()
+{
+
+}
+
+QVector<QVector<int> > Sudoku::generateBoard()
+{
+    for(row_ = 0; row_ < 9; row_++) {
+        QVector<int> numbers = shuffleNumbers({1,2,3,4,5,6,7,8,9});
+        for(column_ = 0; column_ < 9; column_++) {
+            for(int element = 0; element < 9; element++) {
+                if(!checkRow(row_, numbers[element]) && !checkColumn(column_, numbers[element]) && !checkSquare(row_, column_, numbers[element])) {
+                    board_[row_][column_] = numbers[element];
+                    numbers.remove(element, 1);
+                    break;
+                }
+            }
+        }
+        checkGeneratedRow(row_);
+    }
+
+    return board_;
+}
+
+bool Sudoku::checkRow(int row, int number)
+{
+    for(int column = 0; column < 9; column++) {
+        if(board_[row][column] == number) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Sudoku::checkColumn(int column, int number)
+{
+    for(int row = 0; row < 9; row++) {
+        if(board_[row][column] == number) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Sudoku::checkSquare(int row, int column, int number)
+{
+    row = row - row % 3;
+    column = column - column % 3;
+    int maxRow = row + 3;
+    int maxColumn = column + 3;
+
+    for(row; row < maxRow; row++) {
+        for(column; column < maxColumn; column++) {
+            if(board_[row][column] == number) {
+                return true;
+            }
+        }
+        column = maxColumn - 3;
+    }
+    return false;
+}
+
+QVector<int> Sudoku::shuffleNumbers(QVector<int> line)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(line.begin(), line.end(), gen);
+    return line;
+}
+
+void Sudoku::checkGeneratedRow(int row)
+{
+    for(int column = 0; column < 9; column++) {
+        if(board_[row][column] == 0) {
+            resetRow(row);
+            lineFailure_++;
+            if(lineFailure_ == 10) {
+                resetBoard();
+                row_ = -1;
+                lineFailure_ = 0;
+            } else {
+                row_--;
+            }
+        }
+        if(column == 9) {
+            lineFailure_ = 0;
+        }
+    }
+}
+
+void Sudoku::resetRow(int row)
+{
+    board_[row] = {0,0,0,0,0,0,0,0,0};
+}
+
+void Sudoku::resetBoard()
+{
+    board_ = QVector<QVector<int>>(9, QVector<int>(9, 0));
 }
--- src/sudokugenerator.h
+++ src/sudokugenerator.h
@@ -1,11 +1,36 @@
 #ifndef SUDOKUGENERATOR_H
 #define SUDOKUGENERATOR_H
 
+#include <QVector>
+
+
 class Sudoku
 {
 public:
     Sudoku();
+    ~Sudoku();
+
+    QVector<QVector<int>> board_{};
+
+    QVector<QVector<int>> generateBoard();
+
+private:
+    int lineFailure_{0};
+    int row_{0};
+    int column_{0};
+
+    bool checkRow(int row, int number);
+    bool checkColumn(int column, int number);
+    bool checkSquare(int row, int column, int number);
+
+    QVector<int> shuffleNumbers(QVector<int> line);
+    void checkGeneratedRow(int row);
+
+    void resetRow(int row);
+    void resetBoard();
 
 };
 
 #endif // SUDOKUGENERATOR_H
+
+
