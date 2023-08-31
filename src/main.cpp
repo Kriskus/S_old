ed1b26bf4f6b9caa7257307541a65cd758e07aeb
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-08-31 13:30:58 +0200

    Update main.cpp
    Simple test generate sudoku board

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,24 @@
 
 #include <QApplication>
 
+#include "sudokugenerator.h"
+
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    //    QApplication a(argc, argv);
+    //    MainWindow w;
+    //    w.show();
+    //    return a.exec();
+
+    Sudoku s;
+    QVector<QVector<int>> board = s.generateBoard();
+
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            std::cout << QString::number(board[i][j]).toStdString() + " ";
+        }
+        std::cout << "\n";
+    }
 }
