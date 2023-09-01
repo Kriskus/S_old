0ca8ee8ddf708385aee137c5e29af3ab2d32c338
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-09-01 12:34:35 +0200

    Update main.cpp
    Remove test in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,10 @@
 
 #include <QApplication>
 
-#include "sudokugenerator.h"
-
-#include <iostream>
-
 int main(int argc, char *argv[])
 {
-    //    QApplication a(argc, argv);
-    //    MainWindow w;
-    //    w.show();
-    //    return a.exec();
-
-    Sudoku* s = new Sudoku;
-    QVector<QVector<int>> board = s->generateBoard();
-
-    for(int i = 0; i < 9; i++) {
-        for(int j = 0; j < 9; j++) {
-            std::cout << QString::number(board[i][j]).toStdString() + " ";
-        }
-        std::cout << "\n";
-    }
-    delete s;
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
