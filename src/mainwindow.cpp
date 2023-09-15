a5e3c6b3ffd696a22f2f25dfe640aadec0b92762
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-09-15 14:02:40 +0200

    Add buttons

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QPushButton>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -26,16 +28,38 @@ void MainWindow::newGame()
 
 void MainWindow::generateGameBoard()
 {
-        for (int row = 0; row < 9; ++row) {
-            for (int column = 0; column < 9; ++column) {
-                setBoardLayout(row, column);
+    for (int row = 0; row < 9; ++row) {
+        for (int column = 0; column < 9; ++column) {
+            setBoardLayout(row, column);
 
-                Cell* cell = new Cell(sudokuBoard_[row][column]);
-                connect(cell, &Cell::clicked, this, &MainWindow::setCellToEdit);
+            Cell* cell = new Cell(sudokuBoard_[row][column]);
+            connect(cell, &Cell::clicked, this, &MainWindow::setCellToEdit);
 
-                currentBoardLayout_->addCell(cell);
-            }
+            currentBoardLayout_->addCell(cell);
         }
+    }
+    int buttonRow = 0;
+    int buttonColumn = 0;
+    for (int element = 0; element < 10; ++element) {
+        QPushButton* button;
+        if(element < 9) {
+            button = new QPushButton(QString::number(element+1));
+            button->setObjectName(QString::number(element+1));
+        } else {
+            button = new QPushButton("C");
+            button->setObjectName("0");
+        }
+        button->setMinimumSize(60,60);
+        button->setMaximumSize(60,60);
+        if(buttonColumn == 5) {
+            buttonColumn = 0;
+            buttonRow++;
+        }
+        ui->buttonLayout->addWidget(button, buttonRow, buttonColumn++);
+        connect(button, &QPushButton::clicked, [this, button]() {
+                cell_->writeDigit(button->objectName());
+            });
+    }
 }
 
 void MainWindow::createLayout()
