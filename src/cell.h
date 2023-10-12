d28ca215c630d9608decf5fac627b107afe7dc8d
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-10-12 12:12:01 +0200

    Changed cell size to static

--- src/cell.h
+++ src/cell.h
@@ -7,11 +7,12 @@ class Cell : public QLabel
 {
     Q_OBJECT
 public:
-    Cell(int correctDigit = 0, bool visible = false, int sideSize = 0);
+    Cell(int correctDigit = 0, bool visible = false, int difficultyLvl = 0);
     ~Cell();
 
 private:
     int correctDigit_{0};
+    int difficultyLvl_{0};
     QString style_{};
     bool readOnly_{false};
 
@@ -20,7 +21,8 @@ public slots:
     void setStyle(bool active);
 
 private slots:
-    QString checkDigit(bool active);
+    bool checkDigit();
+    QString setFontColor(bool active);
 
 signals:
     void clicked(Cell* cell);
